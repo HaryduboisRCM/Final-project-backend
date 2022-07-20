70ca136ab7b1c4c4942b4aa9005babe19b65f6ab
RachelHB <105057555+RachelHB@users.noreply.github.com>
2022-07-20 12:54:15 +0100

    added access Level for permissions

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   userName: String,
   password: String,
-  token: String
+  token: String,
+  accessLevel: String
 })
 
 module.exports.User = mongoose.model('User', userSchema, 'Users')
