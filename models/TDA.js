965165480fd9fa6bb3ce226b18c96fa536503c8e
RachelHB <105057555+RachelHB@users.noreply.github.com>
2022-08-02 13:35:57 +0100

    updated comments and deleted unused files

--- models/TDA.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const tdaSchema = mongoose.Schema({
-  userName: String,
-  password: String,
-  token: String
-})
-
-module.exports.TDA = mongoose.model('TDA', tdaSchema, 'TDAs')
--- models/employer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const employerSchema = mongoose.Schema({
-  userName: String,
-  password: String,
-  token: String
-})
-
-module.exports.Employer = mongoose.model('Employer', employerSchema, 'Employers')
